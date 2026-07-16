064bbb614ddebb47ee5378779823bd5b6c2bbccc
vishalkumar262006 <vk1490257@gmail.com>
2026-07-16 22:34:38 +0530

    Time: 34 ms (94.24%) | Memory: 62.6 MB (63.36%) - LeetSync

--- 77-combinations/combinations.cpp
+++ 77-combinations/combinations.cpp
@@ -5,13 +5,20 @@ public:
             ans.push_back(temp);
             return;
            }
-           if(start>n){
-            return;
-           }
-           temp.push_back(start);
-           solve(start+1,temp,ans,k-1,n);
-           temp.pop_back();
-           solve(start+1,temp,ans,k,n);
+           //khandnai tempalte wala
+        //    if(start>n){
+        //     return;
+        //    }
+        //    temp.push_back(start);
+        //    solve(start+1,temp,ans,k-1,n);
+        //    temp.pop_back();
+        //    solve(start+1,temp,ans,k,n);
+        //for loop wala;
+       for(int i=start;i<=n;i++){
+         temp.push_back(i);
+         solve(i+1,temp,ans,k-1,n);
+         temp.pop_back();
+       }
 
     }
     vector<vector<int>> combine(int n, int k) {
