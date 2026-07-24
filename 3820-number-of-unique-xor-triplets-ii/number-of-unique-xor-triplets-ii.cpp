c6a415e105b83825c1061c876e97d5de0d84d0ac
vishalkumar262006 <vk1490257@gmail.com>
2026-07-24 22:32:09 +0530

    Time: 639 ms (64.61%) | Memory: 42.5 MB (66.85%) - LeetSync

--- 3820-number-of-unique-xor-triplets-ii/number-of-unique-xor-triplets-ii.cpp
+++ 3820-number-of-unique-xor-triplets-ii/number-of-unique-xor-triplets-ii.cpp
@@ -2,20 +2,47 @@ class Solution {
 public:
     int uniqueXorTriplets(vector<int>& nums) {
         int n=nums.size();
-        //store all pair xors
-        unordered_set<int>s1;
+        // //store all pair xors
+        // unordered_set<int>s1;
+        // for(int i=0;i<n;i++){
+        //     for(int j=i;j<n;j++){
+        //         s1.insert(nums[i]^nums[j]);
+        //     }
+        // }
+        // //aab pair xor ka nums ke sath xor
+        // unordered_set<int>s2;
+        // for(int pairxor:s1 ){
+        //     for(int num:nums){
+        //         s2.insert(pairxor^num);
+        //     }
+        // }
+        // return s2.size();
+        int maxel= *max_element(begin(nums),end(nums));
+        int t=1;
+        while(t<=maxel){
+            t*=t*2;
+        }
+        vector<bool>s1(t,false);
+        vector<bool>s2(t,false);
         for(int i=0;i<n;i++){
             for(int j=i;j<n;j++){
-                s1.insert(nums[i]^nums[j]);
+                s1[nums[i]^nums[j]]=true;
             }
         }
-        //aab pair xor ka nums ke sath xor
-        unordered_set<int>s2;
-        for(int pairxor:s1 ){
-            for(int num:nums){
-                s2.insert(pairxor^num);
+        for(int i=0;i<t;i++){
+            if(s1[i]==true){
+                for(int chikki:nums){
+                    s2[i^chikki]=true;
+                }
             }
         }
-        return s2.size();
+        int count=0;
+        for(int i=0;i<t;i++){
+            if(s2[i]==true){
+                count++;
+            }
+        }
+        return count;
+
     }
 };
