1fd7a033a123b4501e0e7f9d6886b31d4c4b74c2
vishalkumar262006 <vk1490257@gmail.com>
2026-07-31 15:06:40 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (31.56%) - LeetSync

--- 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,16 +1,28 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        unordered_map<int,int>mp;
+        // unordered_map<int,int>mp;
+        // int result=0;
+        // int start_key=2;
+        // for(char ch:word){
+        //     if(start_key>9){
+        //         start_key=2;
+        //     }
+        //     mp[start_key]++;
+        //     result+=mp[start_key];
+        //     start_key++;
+        // }
+        // return result;
         int result=0;
-        int start_key=2;
+        vector<int>mp(26,0);
         for(char ch:word){
-            if(start_key>9){
-                start_key=2;
-            }
-            mp[start_key]++;
-            result+=mp[start_key];
-            start_key++;
+            mp[ch-'a']++;
+        }
+        sort(mp.begin(),mp.end(),greater<int>());
+        for(int i=0;i<26;i++){
+            int freq=mp[i];
+            int press= i/8+1;
+            result+=press*freq;
         }
         return result;
     }
