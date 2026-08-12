5bece63fdfbfb898c38723545710f7e44dc3649b
vishalkumar262006 <vk1490257@gmail.com>
2026-08-12 16:16:39 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (27.05%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -10,30 +10,46 @@
  */
 class Solution {
 public:
-    int lengthlinkedlist(ListNode* head){
-        int s=0;
-        while(head){
-            s++;
-            head=head->next;
-        }
-        return s;
-    }
+    // int lengthlinkedlist(ListNode* head){
+    //     int s=0;
+    //     while(head){
+    //         s++;
+    //         head=head->next;
+    //     }
+    //     return s;
+    // }
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        int length= lengthlinkedlist(head);
-        if(length==n){
-            ListNode*temp=head->next;
-            delete(head);
-            return temp;
-        }
+        // int length= lengthlinkedlist(head);
+        // if(length==n){
+        //     ListNode*temp=head->next;
+        //     delete(head);
+        //     return temp;
+        // }
+        // ListNode*temp=head;
+        // ListNode*prev=NULL;
+        // int traversal= length-n;
+        // while(traversal--){
+        //     prev=temp;
+        //     temp=temp->next;
+        // }
+        // prev->next=temp->next;
+        // delete temp;
+        // return head;
         ListNode*temp=head;
-        ListNode*prev=NULL;
-        int traversal= length-n;
-        while(traversal--){
-            prev=temp;
+        for(int i=0;i<n;i++){
             temp=temp->next;
         }
-        prev->next=temp->next;
-        delete temp;
+        if(temp==NULL){
+            ListNode*temp=head->next;
+            delete head;
+            return temp;
+        }
+        ListNode*prev=head;
+        while(temp!=NULL && temp->next!=NULL){
+            prev=prev->next;
+            temp=temp->next;
+        }
+        prev->next=prev->next->next;
         return head;
     }
 };
